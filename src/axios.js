580acb55dc1688193ea4023d9eabdebad5ea81ed
old-shoes <2916331079@qq.com>
2019-01-22 08:35:10 +0800

    xiugai

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios"
-const baseURL=process.env.NODE_ENV=="development" ? "http://localhost:8080" : ""
+const baseURL=process.env.NODE_ENV=="development" ? "http://ele.kassing.cn" : ""
 axios.defaults.baseURL=baseURL
 export default axios
