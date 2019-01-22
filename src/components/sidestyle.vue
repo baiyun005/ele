57919803ba122076e4de65b37bf993173edf8e9c
Burningys <1134526248.@qq.com>
2019-01-22 09:36:47 +0800

    创建components  创建商家店铺侧边组件

---
+++ src/components/sidestyle.vue
@@ -0,0 +1,46 @@
+<template>
+    <cube-scroll-nav
+    :side="true"
+    :data="data"
+    :current="current"
+    @change="changeHandler"
+    @sticky-change="stickyChangeHandler">
+    <cube-scroll-nav-panel
+        v-for="item in data"
+        :key="item.name"
+        :label="item.name"
+        :title="item.name">
+        <ul>
+        <li v-for="(food,index) in item.foods" :key="index">
+            <div>
+                <p>{{food.name}}</p>
+            </div>
+        </li>
+        </ul>
+    </cube-scroll-nav-panel>
+    </cube-scroll-nav>
+</template>
+
+<script>
+import axios from '../axios'
+export default {
+    data(){
+
+    },
+    methods:{
+        changeHandler(){
+
+        },
+        stickyChangeHandler(){
+
+        }
+    },
+    created(){
+        this.$store.dispatch('getdata')
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,18 @@ import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 const store = new Vuex.Store({
-  state:{},
-  getters:{},
-  mutations:{},
-  actions:{},
+  state:{
+
+  },
+  getters:{
+
+  },
+  mutations:{
+    
+  },
+  actions:{
+
+  }, 
   modules:{}
 })
 export default store
